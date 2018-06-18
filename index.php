ffcbe59c3e428f0ac84735fff7bd00d7bcb75ceb
Rushil Ramautar <rushil.ramautar@gmail.com>
2018-06-18 14:46:55 -0300

    added dynamic time selector for time selecting

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <?php require 'assets/includes/header.php' ?>
 
+<?php
+
+  $connection = mysqli_connect("localhost", "gpadmin", "gpadmin321", "guestpass");
+  if (!$connection) {
+    die("Connection Failed" . mysqli_connect_error());
+  }
+  $rollQuery = "select  distinct
+  rolls.roll_id,
+  rolls.roll_desc,
+  rolls.roll_time
+  from rolls;";
+  $rollResult = mysqli_query($connection, $rollQuery);
+
+
+?>
+
 <div class="container p-4">
   <center>
     <div class="rounded bg-dark text-light px-4 py-4" id="voucherPanel">
       <center>
         <h1>GuestPass</h1>
-        <form action="test.php" method="post">
+        <form action="index.php" method="post" autocomplete="off">
           <div class="form-group">
             <label for="voucherSelect">Tijd per Voucher</label>
             <select class="form-control" id="voucherSelect" name="minutes">
-              <option value="120">2 Uren</option>
-              <option value="480">8 Uren</option>
-              <option value="4320">3 Dagen</option>
-              <option value="10080">7 Dagen</option>
+              <?php
+
+              if (mysqli_num_rows($rollResult) > 0) {
+                while ($rollRow = mysqli_fetch_assoc($rollResult)) {
+                  echo '<option value="' . $rollRow['roll_time'] . '">' . $rollRow['roll_desc'] . "</option>";
+                }
+              }else {
+                echo '<option>Error loading database</option>';
+              }
+
+              ?>
             </select>
           </div>
           <div class="form-group">
@@ -30,5 +53,11 @@
     </div>
   </center>
 </div>
+
+<?php
+
+  mysqli_close($connection);
+
+?>
 
 <?php require 'assets/includes/footer.php' ?>
